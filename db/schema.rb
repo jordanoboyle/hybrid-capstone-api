3d625ab130c9c95cbdd48bb7fe7bf5887c49af3d
jordanoboyle <jmcmcode@gmail.com>
2024-07-10 13:57:20 -0600

    genre model, sub_genre model, systems model; users seed data test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_192411) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_10_194605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_10_192411) do
     t.integer "genre_id"
     t.integer "system_id"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -44,6 +51,21 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_10_192411) do
     t.string "systems"
     t.text "body"
     t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sub_genres", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "systems", force: :cascade do |t|
+    t.string "name"
+    t.string "company"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(first_name: "Luke", last_name: "Skywalker", email: "luke@email.com", username: "luke2011", password: "password", password_confirmation: "password", admin: true, prof_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyimGSp2axjfAaKtnXTutok7ylEgwGdQvXDw&s", about_me: "Flavor text about yourself can be put here. Share with the world what makes you you!")
+User.create(first_name: "Cloud", last_name: "Strife", email: "cloud@email.com", username: "cloud2011", password: "password", password_confirmation: "password", admin: false, prof_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyimGSp2axjfAaKtnXTutok7ylEgwGdQvXDw&s", about_me: "Flavor text about yourself can be put here. Share with the world what makes you you!")
+User.create(first_name: "Tink", last_name: "OBoyle", email: "tink@email.com", username: "tink2011", password: "password", password_confirmation: "password", admin: false, prof_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyimGSp2axjfAaKtnXTutok7ylEgwGdQvXDw&s", about_me: "Flavor text about yourself can be put here. Share with the world what makes you you!")
