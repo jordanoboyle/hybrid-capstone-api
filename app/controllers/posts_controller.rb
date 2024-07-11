fc12fabc1e36e47de98e7b8da4d693b516735933
jordanoboyle <jmcmcode@gmail.com>
2024-07-11 12:51:05 -0600

    create action, update action posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,5 +10,35 @@ class PostsController < ApplicationController
     render template: "posts/show"
   end
 
+  def create
+    @post = Post.new(
+    user_id: params[:user_id],  ## Modify to current_user_id
+    title: params[:title],
+    game_title: params[:game_title],
+    genre_id: params[:genre_id],
+    system_id: params[:system_id],
+    body: params[:body]
+    )
+    if @post.save
+      render template: "posts/show"
+    else
+      render json: { ERRORs: @post.errors.full_messages }
+    end
+  end
 
+  def update
+    @post = Post.find_by(id: 13)
+    @post.user_id = params[:user_id]  || @post.user_id
+    @post.title = params[:title]  || @post.title
+    @post.game_title = params[:game_title]  || @post.game_title
+    @post.genre_id = params[:genre_id]  || @post.genre_id
+    @post.system_id = params[:system_id]  || @post.system_id
+    @post.body = params[:body]  || @post.body
+
+    if @post.save
+      render template: "posts/show"
+    else
+      render json: {message: @post.errors.full_messages}
+    end
+  end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -20,4 +20,11 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal ["id", "title", "game_title", "genre_id", "system_id", "body", "created_at", "updated_at", "genre", "system", "user"], data.keys
   end
+
+  test "create" do
+    assert_difference "Post.count", 1 do
+      post "/posts.json", params: { user_id: 1, title: "Some Title", game_title: "test", genre_id: 1, system_id: 1, body: "Some Text"}
+      assert_response 200
+    end
+  end
 end
