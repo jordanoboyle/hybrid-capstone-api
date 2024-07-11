0c4a15201485e7ac8fdc3c31d42fe525acbd72b7
jordanoboyle <jmcmcode@gmail.com>
2024-07-11 11:47:45 -0600

    authentication admin and user restrictions, needs current user restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  # before_action :authenticate_admin, only: [:index]
+  # before_action :authenticate_user, only: [:update, :destroy]
 
   def create
     user = User.new(
